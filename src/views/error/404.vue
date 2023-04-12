3f783077d1605e6419f0669b9b6019b65fae75b4
TsMask <340112800@qq.com>
2023-04-12 15:52:52 +0800

    style: 错误页面改用结果页组件

--- src/views/error/404.vue
+++ src/views/error/404.vue
@@ -1,17 +1,22 @@
 <template>
-  <a-row :gutter="20">
-    <a-col :span="12">
-      <img src="@/assets/images_error/404.svg" width="313" height="428" alt="404!"
-    /></a-col>
-    <a-col :span="12">
-      <h1 class="text-jumbo text-ginormous">404错误!</h1>
-      <h2>找不到网页！</h2>
-      <h6>
-        对不起，您正在寻找的页面不存在。尝试检查URL的错误，然后按浏览器上的刷新按钮或尝试在我们的应用程序中找到其他内容。
-      </h6>
-      <a-button type="primary" href="/index"> 返回首页 </a-button>
-    </a-col>
-  </a-row>
+  <a-result
+    status="404"
+    title="找不到匹配页面"
+    sub-title="对不起，您正在寻找的页面不存在。"
+  >
+    <template #extra>
+      <a-button type="primary" href="/index">返回首页</a-button>
+    </template>
+    <a-typography>
+      <a-typography-title> 找不到网页？ </a-typography-title>
+      <a-typography-paragraph>
+        1. 尝试检查URL的错误，然后按浏览器上的刷新按钮。
+      </a-typography-paragraph>
+      <a-typography-paragraph>
+        2. 尝试在我们的应用程序中找到其他内容。
+      </a-typography-paragraph>
+    </a-typography>
+  </a-result>
 </template>
 
 <script setup lang="ts"></script>
