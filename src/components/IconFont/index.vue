41f46ee8dc084e4298da485cecde2afd01c5e278
TsMask <340112800@qq.com>
2023-04-20 11:39:43 +0800

    feat: 自定义字体图标组件

---
+++ src/components/IconFont/index.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { scriptUrl } from '@/assets/js/icon_font_8d5l8fzk5b87iudi';
+import { createFromIconfontCN } from '@ant-design/icons-vue';
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: '#',
+  },
+  size: {
+    type: Number,
+    default: 14,
+  },
+});
+
+/**字体图标加载为组件 */
+const IconFont = createFromIconfontCN({
+  scriptUrl: scriptUrl,
+});
+</script>
+
+<template>
+  <IconFont
+    v-if="type != '#'"
+    :type="props.type"
+    :style="{ fontSize: size + 'px' }"
+  ></IconFont>
+</template>
+
+<style lang="less" scoped></style>
