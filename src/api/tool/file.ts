efbb4a4a681b6511461527a1e7556b51db1d149a
TsMask <340112800@qq.com>
2023-06-09 19:15:49 +0800

    feat: 文件上传操作

---
+++ src/api/tool/file.ts
@@ -0,0 +1,195 @@
+import { request } from '@/plugins/http-fetch';
+import { encode } from 'js-base64';
+
+/**
+ * 下载文件
+ * @param filePath 文件路径带/，如:/upload/default/2023/06/xx.png
+ * @param range 断点续传标识，填入字符串 `bytes=${startByte}-${endByte}`
+ * @returns object
+ */
+export async function downloadFile(filePath: string, range?: string) {
+  return request({
+    url: `/file/download/${encode(filePath)}`,
+    method: 'get',
+    headers: range ? { range } : {},
+    responseType: 'blob',
+  });
+}
+
+/**
+ * 下载文件切片
+ * @param filePath 文件路径带/，如:/upload/default/2023/06/xx.png
+ * @param chunkSize 数据块大小MB，默认1MB
+ * @returns bolb
+ */
+export async function downloadFileChunk(
+  filePath: string,
+  chunkSize: number = 1
+): Promise<Blob> {
+  chunkSize = chunkSize * 1024 * 1024;
+  let start = 0; // 文件块的起始字节
+  let end = chunkSize - 1; // 文件块的结束字节
+  let totalSize = 0; // 文件总大小
+  let downloadedSize = 0; // 已下载的文件大小
+  let filePart: Blob[] = []; // 文件数据块内容
+
+  // 发送带有 Range 请求头的 HTTP 请求
+  async function sendRequest() {
+    const range = `bytes=${start}-${end}`;
+    const res = await downloadFile(filePath, range);
+    if (res.code === 200 && res.status === 206) {
+      // 总大小
+      const contentRange = res.headers.get('content-range') || '0/0';
+      totalSize = parseInt(contentRange.split('/')[1]);
+      // 已下载大小
+      const contentLength = res.headers.get('content-length') || '0';
+      const chunkSize = parseInt(contentLength);
+      // 下一段数据块区间
+      start += chunkSize;
+      end = Math.min(start + chunkSize - 1, totalSize - 1);
+      // 记录下载结果
+      filePart.push(res.data);
+      downloadedSize += chunkSize;
+      // 小于总大小继续下载后续数据
+      if (downloadedSize < totalSize) {
+        await sendRequest();
+      }
+    } else {
+      return res;
+    }
+  }
+
+  await sendRequest();
+  return new Blob(filePart, { type: 'application/octet-stream' });
+}
+
+/**
+ * 上传文件
+ * @param data 表单数据对象
+ * @returns object
+ */
+export function uploadFile(data: FormData) {
+  return request({
+    url: '/file/upload',
+    method: 'post',
+    data,
+    dataType: 'form-data',
+  });
+}
+
+/**
+ * 上传切片文件
+ * @param file 文件对象
+ * @param chunkSize 数据块大小MB，默认1MB
+ * @param subPath 归属子路径, 默认default
+ * @returns
+ */
+export async function uploadFileChunk(
+  fileData: File,
+  chunkSize: number = 1,
+  subPath: string = 'default'
+) {
+  const { name, size } = fileData;
+  const chunkSizeInBytes = chunkSize * 1024 * 1024;
+  // 文件标识使用唯一编码 MD5(文件名+文件大小)
+  const fileIdentifier = `${name}-${size}`;
+  // 文件切分为多少份进行上传
+  const chunksCount = Math.ceil(size / chunkSizeInBytes);
+  // 切块的数据数据用于上传
+  const fileChunks: { index: number; chunk: Blob }[] = [];
+
+  for (let i = 0; i < chunksCount; i++) {
+    const start = i * chunkSizeInBytes;
+    const end = Math.min(start + chunkSizeInBytes, size);
+    fileChunks.push({
+      index: i,
+      chunk: fileData.slice(start, end),
+    });
+  }
+
+  // 检查是否已上传部分数据块
+  const resCheck = await chunkCheck(fileIdentifier, name);
+  if (resCheck.code !== 200) {
+    return resCheck;
+  }
+
+  let uploadedSize = 0;
+  let uploadProgress = 0;
+
+  for (const { index, chunk } of fileChunks) {
+    const chunksIndex = `${index}`;
+    // 跳过已上传块
+    if (resCheck.data.includes(chunksIndex)) {
+      continue;
+    }
+
+    // 上传数据块
+    const formData = new FormData();
+    formData.append('file', chunk, name);
+    formData.append('index', chunksIndex);
+    formData.append('identifier', fileIdentifier);
+
+    const resUpload = await chunkUpload(formData);
+    if (resUpload.code === 200) {
+      uploadedSize += chunk.size;
+      uploadProgress = (uploadedSize / size) * 100;
+      console.log(`上传进度：${uploadProgress}%`);
+    } else {
+      // 上传失败处理
+      break;
+    }
+  }
+
+  // 上传数据完整后合并数据块
+  if (uploadedSize === size) {
+    return await chunkMerge(fileIdentifier, name, subPath);
+  }
+  return { code: 500, msg: '上传出错，请重试' };
+}
+
+/**
+ * 切片文件检查
+ * @param identifier 文件标识
+ * @param fileName 原文件名称
+ * @returns object
+ */
+export function chunkCheck(identifier: string, fileName: string) {
+  return request({
+    url: '/file/chunkCheck',
+    method: 'post',
+    data: { identifier, fileName },
+  });
+}
+
+/**
+ * 切片文件合并
+ * @param identifier 文件标识
+ * @param fileName 原文件名称
+ * @param subPath 文件归属
+ * @returns object
+ */
+export function chunkMerge(
+  identifier: string,
+  fileName: string,
+  subPath: string = 'default'
+) {
+  return request({
+    url: '/file/chunkMerge',
+    method: 'post',
+    data: { identifier, fileName, subPath },
+  });
+}
+
+/**
+ * 切片文件上传
+ * @param data 表单数据对象
+ * @returns object
+ */
+export function chunkUpload(data: FormData) {
+  return request({
+    url: '/file/chunkUpload',
+    method: 'post',
+    data,
+    dataType: 'form-data',
+  });
+}
---
+++ src/views/tool/upload/index.vue
@@ -0,0 +1,234 @@
+<script lang="ts" setup>
+import {
+  downloadFile,
+  downloadFileChunk,
+  uploadFile,
+  uploadFileChunk,
+} from '@/api/tool/file';
+import { Modal } from 'ant-design-vue/lib/components';
+import message from 'ant-design-vue/lib/message';
+import { FileType, UploadFile } from 'ant-design-vue/lib/upload/interface';
+import { UploadRequestOption } from 'ant-design-vue/lib/vc-upload/interface';
+import saveAs from 'file-saver';
+import { reactive } from 'vue';
+
+let state = reactive<{
+  /**上传状态 */
+  loading: boolean;
+  uploadFilePath: string;
+  downloadFilePath: string;
+  /*文件列表 */
+  fileList: UploadFile<any>[];
+}>({
+  loading: false,
+  uploadFilePath: '',
+  downloadFilePath: '',
+  fileList: [
+    // {
+    //   uid: '1',
+    //   percent: 100,
+    //   status: 'success',
+    //   name: 'xxx.png',
+    //   url: '/upload/default/2023/06/xxx.png',
+    //   thumbUrl: '/upload/default/2023/06/xxx.png',
+    // },
+  ],
+});
+
+/**下载文件 */
+function fnDownload() {
+  const key = 'downloadFile';
+  message.loading({ content: '请稍等...', key });
+  const filePath = state.downloadFilePath;
+  if (!filePath) return;
+  downloadFile(filePath).then(res => {
+    if (res.code === 200) {
+      message.success({
+        content: `已完成下载`,
+        key,
+        duration: 2,
+      });
+      const fileName = filePath.substring(filePath.lastIndexOf('/') + 1);
+      saveAs(res.data, fileName);
+    } else {
+      message.error({
+        content: `${res.msg}`,
+        key,
+        duration: 2,
+      });
+    }
+  });
+}
+
+/**下载切片文件 */
+function fnDownloadChunk() {
+  const key = 'downloadFileChunk';
+  message.loading({ content: '请稍等...', key });
+  const filePath = state.downloadFilePath;
+  downloadFileChunk(filePath, 5).then(blob => {
+    console.log(blob);
+    if (blob.size === 0) {
+      message.error({
+        content: `文件读取失败`,
+        key,
+        duration: 2,
+      });
+    } else {
+      message.success({
+        content: `已完成下载`,
+        key,
+        duration: 2,
+      });
+      const fileName = filePath.substring(filePath.lastIndexOf('/') + 1);
+      saveAs(blob, fileName);
+    }
+  });
+}
+
+/**上传前检查或转换压缩 */
+function fnBeforeUpload(file: FileType) {
+  if (state.loading) return false;
+  const isJpgOrPng = ['image/jpeg', 'image/png'].includes(file.type);
+  if (!isJpgOrPng) {
+    message.error('只支持上传图片格式（jpg、png）', 3);
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error('图片文件大小必须小于 2MB', 3);
+  }
+  return isJpgOrPng && isLt2M;
+}
+
+/**上传文件 */
+function fnUpload(up: UploadRequestOption) {
+  Modal.confirm({
+    title: '提示',
+    content: `确认要上传文件吗?`,
+    onOk() {
+      // 发送请求
+      const hide = message.loading('请稍等...', 0);
+      state.loading = true;
+      let formData = new FormData();
+      formData.append('file', up.file);
+      formData.append('subPath', 'default');
+      uploadFile(formData).then(res => {
+        state.loading = false;
+        hide();
+        if (res.code === 200) {
+          message.success('文件上传成功', 3);
+          state.uploadFilePath = res.data.url;
+          state.downloadFilePath = res.data.fileName;
+        } else {
+          message.error(res.msg, 3);
+        }
+      });
+    },
+  });
+}
+
+/**上传分片 */
+function fnUploadChunk(up: UploadRequestOption) {
+  const fileData = up.file as File;
+  const item = state.fileList.find(f => f.name === fileData.name);
+  Modal.confirm({
+    title: '提示',
+    content: `确认要上传文件吗?`,
+    onOk() {
+      // 发送请求
+      const hide = message.loading('请稍等...', 0);
+      uploadFileChunk(fileData, 4, 'default').then(res => {
+        hide();
+        if (res.code === 200) {
+          message.success('文件上传成功', 3);
+          if (item) {
+            item.url = res.data.url;
+            item.name = res.data.newFileName;
+            item.percent = 100;
+            item.status = 'done';
+          }
+        } else {
+          message.error(res.msg, 3);
+        }
+      });
+    },
+    onCancel() {
+      if (item) {
+        state.fileList.splice(state.fileList.length - 1, 1);
+      }
+    },
+  });
+}
+</script>
+
+<template>
+  <page-container title="上传示例">
+    <a-row :gutter="16">
+      <a-col :lg="12" :md="12" :xs="24">
+        <a-card title="普通文件" style="margin-bottom: 16px">
+          <a-row :gutter="8">
+            <a-col :span="24" style="margin-bottom: 16px">
+              <a-input
+                style="margin-bottom: 16px"
+                type="text"
+                placeholder="输入资源文件地址"
+                v-model:value="state.downloadFilePath"
+              >
+                <template #suffix>
+                  <a-button type="primary" @click="fnDownload">
+                    普通下载
+                  </a-button>
+                </template>
+              </a-input>
+              <a-input
+                type="text"
+                placeholder="输入资源文件地址"
+                v-model:value="state.downloadFilePath"
+              >
+                <template #suffix>
+                  <a-button type="primary" @click="fnDownloadChunk">
+                    分片下载
+                  </a-button>
+                </template>
+              </a-input>
+            </a-col>
+            <a-col :span="24">
+              <a-space direction="vertical" :size="16">
+                <a-upload
+                  name="file"
+                  list-type="picture"
+                  :max-count="1"
+                  :show-upload-list="false"
+                  :before-upload="fnBeforeUpload"
+                  :custom-request="fnUpload"
+                >
+                  <a-button type="default" :loading="state.loading">
+                    选择文件
+                  </a-button>
+                </a-upload>
+                <a-image
+                  :width="128"
+                  :height="128"
+                  :src="state.uploadFilePath"
+                />
+              </a-space>
+            </a-col>
+          </a-row>
+        </a-card>
+      </a-col>
+      <a-col :lg="12" :md="12" :xs="24">
+        <a-card title="大文件分片上传" style="margin-bottom: 16px">
+          <a-upload
+            v-model:file-list="state.fileList"
+            name="file"
+            list-type="picture"
+            :custom-request="fnUploadChunk"
+          >
+            <a-button> 选择文件 </a-button>
+          </a-upload>
+        </a-card>
+      </a-col>
+    </a-row>
+  </page-container>
+</template>
+
+<style lang="less" scoped></style>
