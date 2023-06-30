3b1a7d0f4b78154d332f8a7bbf177bac7d68010c
TsMask <340112800@qq.com>
2023-06-30 16:58:58 +0800

    feat: 定义存放Cookie标识常量

--- src/constants/token-constants.ts
+++ src/constants/token-constants.ts
@@ -6,3 +6,6 @@ export const TOKEN_KEY_PREFIX = 'Bearer ';
 
 /**令牌-请求头标识 */
 export const TOKEN_KEY = 'Authorization';
+
+/**令牌-存放Cookie标识 */
+export const TOKEN_COOKIE = 'AuthAntdv';
--- src/plugins/auth-token.ts
+++ src/plugins/auth-token.ts
@@ -1,17 +1,17 @@
-import { TOKEN_KEY } from '@/constants/token-constants';
 import Cookies from 'js-cookie';
+import { TOKEN_COOKIE } from '@/constants/token-constants';
 
 /**获取cookis中Token字符串 */
 export function getToken(): string {
-  return Cookies.get(TOKEN_KEY) || '';
+  return Cookies.get(TOKEN_COOKIE) || '';
 }
 
 /**设置cookis中Token字符串 */
 export function setToken(token: string): void {
-  Cookies.set(TOKEN_KEY, token);
+  Cookies.set(TOKEN_COOKIE, token);
 }
 
 /**移除cookis中Token字符串 */
 export function removeToken(): void {
-  Cookies.remove(TOKEN_KEY);
+  Cookies.remove(TOKEN_COOKIE);
 }
