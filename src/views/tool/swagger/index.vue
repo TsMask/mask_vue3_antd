96fe887f0bc48dead0857b8583b6ab6b5772fdd9
TsMask <340112800@qq.com>
2023-09-03 20:48:14 +0800

    docs: 页面展示接口文档地址变更

--- src/views/tool/swagger/index.vue
+++ src/views/tool/swagger/index.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue';
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 const url = ref<string>(`${baseUrl}/swagger-ui/index.html`);
+url.value = 'https://mask-api-midwayjs.apifox.cn/';
 </script>
 
 <template>
