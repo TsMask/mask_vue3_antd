3ed02947d0866680b37445ba8c0df57ef87919e0
TsMask <340112800@qq.com>
2023-12-30 22:45:10 +0800

    fix: 调整用户选择搜索重置页码

--- src/views/system/role/components/auth-user-select.vue
+++ src/views/system/role/components/auth-user-select.vue
@@ -256,14 +256,14 @@ watch(
         <a-col :lg="12" :md="12" :xs="24">
           <a-form-item>
             <a-space :size="8">
-              <a-button type="primary" @click.prevent="fnGetList">
+              <a-button type="primary" @click.prevent="fnGetList(1)">
                 <template #icon><SearchOutlined /></template>
-                搜索</a-button
-              >
+                搜索
+              </a-button>
               <a-button type="default" @click.prevent="fnQueryReset">
                 <template #icon><ClearOutlined /></template>
-                重置</a-button
-              >
+                重置
+              </a-button>
             </a-space>
           </a-form-item>
         </a-col>
