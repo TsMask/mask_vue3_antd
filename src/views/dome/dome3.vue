bc9da3ad9851e2842bc34cba1f9232ddbd4bb694
TsMask <340112800@qq.com>
2024-05-26 18:09:46 +0800

    feat: 演示示例调整

--- src/views/dome/dome3.vue
+++ src/views/dome/dome3.vue
@@ -1,18 +1,29 @@
 <template>
-  <PageContainer title="Version" sub-title="show current project dependencies">
-    <template #content>
-      <strong>Content Area</strong>
-    </template>
+  <PageContainer
+    title="Title"
+    sub-title="show current project dependencies"
+    :avatar="{
+      src: 'https://gw.alipayobjects.com/zos/antfincdn/K%24NnlsB%26hz/pageHeader.svg',
+    }"
+    @back="() => $router.go(-1)"
+    :ghost="false"
+    :flex="true"
+    :loading="loading"
+  >
     <template #extra>
       <strong>Extra Area</strong>
-    </template>
-    <template #extraContent>
-      <strong>ExtraContent Area</strong>
     </template>
     <template #tags>
       <a-tag>Tag1</a-tag>
       <a-tag color="pink">Tag2</a-tag>
     </template>
+    <template #content>
+      <strong>Content Area</strong>
+    </template>
+    <template #contentExtra>
+      <strong>ContentExtra Area</strong>
+    </template>
+
     <a-card title="Info">
       <p v-for="i in list" :key="i">
         text block...
@@ -23,8 +34,13 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
-import { PageContainer } from 'antdv-pro-layout';
+import { ref } from "vue";
+import { PageContainer } from "antdv-pro-layout";
 
 const list = ref<number>(50);
+
+const loading = ref<boolean>(true);
+setTimeout(() => {
+  loading.value = false
+}, 1200);
 </script>
